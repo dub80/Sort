15674e9d675ec49988f7c4361a4a9a16a71e4caa
Maksym Dub <61121292+dub80@users.noreply.github.com>
2020-09-12 16:13:16 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication3/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication3
+{
+    
+    class Program
+    {
+
+        public static void QuickSort(int[] array)
+        {
+            QuickSort(array, 0, array.Length - 1);
+        }
+        public static void QuickSort (int[] array, int left, int right)
+        {
+            int i = left, j = right;
+            int pivot = array[(left + right) >> 1];
+
+            while (i <= j)
+            {
+                while (array[i] < pivot)
+                {
+                    i++;
+                }
+
+                while (array[j] > pivot)
+                {
+                    j--;
+                }
+
+                if (i <= j)
+                {
+                    // Swap
+                    int tmp = array[i];
+                    array[i] = array[j];
+                    array[j] = tmp;
+
+                    i++;
+                    j--;
+                }
+            }
+
+            // Recursive calls
+            if (left < j)
+            {
+                QuickSort(array, left, j);
+            }
+
+            if (i < right)
+            {
+                QuickSort(array, i, right);
+            }
+        }
+           
+        
+        static void Main(string[] args)
+        {
+                       
+            Console.WriteLine("Алгоритм быстрой сортировки");
+
+            int[] array = { 3, 7, 4, 4, 6, 5, 8, 12, 19, 2, 0 };
+
+            QuickSort(array);
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine(array[i]);
+            }
+
+        }
+    }
+}
